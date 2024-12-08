46864edb8faa121a988266b22fcfe19c1d1801ba
Marcin Jeznach <scheissig@gmail.com>
2024-12-08 13:48:58 +0100

    zadanie 4: ScheiBig

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 print("2024 - Drop_Database", "SCRUM MASTER: Electronc", "DEVELOPER: ScheiBig", "DEVELOPER: lyszkowski-dominik", "Tester: DawidBachanski")
+
+class IArithmeticMult:
+	def Multiplication(self, a: float, b: float) -> float:
+		pass
