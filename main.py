f8bfa360abc264fa49f403209ffd3ce729ff91d2
Dawid <241182@edu.p.lodz.pl>
2024-12-08 13:53:15 +0100

    dodanie operacji addition

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 print("2024 - Drop_Database", "SCRUM MASTER: Electronc", "DEVELOPER: ScheiBig", "DEVELOPER: lyszkowski-dominik", "Tester: DawidBachanski")
+
+
+class IArithmeticAdd:
+    def Addition(self, a: float, b: float) -> float:
+		pass
