59fc18112d98fc63dd0a1366383f5bce7b76fe0c
Electronc <electroncmanmine@gmail.com>
2024-12-08 13:48:51 +0100

    Dodanie funkcji IArithmeticDiff

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 print("2024 - Drop_Database", "SCRUM MASTER: Electronc", "DEVELOPER: ScheiBig", "DEVELOPER: lyszkowski-dominik", "Tester: DawidBachanski")
+
+class IArithmeticsDiff:
+    def diff(self, a: float , b: float) -> float:
+        pass
