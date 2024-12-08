c0c273c6299698718d21f4c0cd284f9c304776e4
Dawid <241182@edu.p.lodz.pl>
2024-12-08 13:53:15 +0100

    cherry-pick f8bfa36

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 print("2024 - Drop_Database", "SCRUM MASTER: Electronc", "DEVELOPER: ScheiBig", "DEVELOPER: lyszkowski-dominik", "Tester: DawidBachanski")
 
-class IArithmeticMult:
-	def Multiplication(self, a: float, b: float) -> float:
+
+class IArithmeticAdd:
+    def Addition(self, a: float, b: float) -> float:
 		pass
