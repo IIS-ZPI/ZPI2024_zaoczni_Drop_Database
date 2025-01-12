a245d239835bd07cb8fa686b2e30f9dbbf48d9d7
Electronc <electroncmanmine@gmail.com>
2025-01-12 14:21:19 +0100

    Clean

--- main.py
+++
@@ -1,6 +0,0 @@
-print("2024 - Drop_Database", "SCRUM MASTER: Electronc", "DEVELOPER: ScheiBig", "DEVELOPER: lyszkowski-dominik", "Tester: DawidBachanski")
-
-
-class IArithmeticAdd:
-    def Addition(self, a: float, b: float) -> float:
-		pass
