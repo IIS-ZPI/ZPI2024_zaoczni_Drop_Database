684938d776dfdc074f48c318c6df5fc677bd7134
Marcin Jeznach <scheissig@gmail.com>
2025-01-23 18:41:25 +0100

    fix formatting and ignored files

--- src/nbp/data_types.py
+++ src/nbp/data_types.py
@@ -6,15 +6,16 @@ exchange_rates = t.Dict[date, float]
 
 currency_code = t.Literal["USD", "EUR", "GBP", "NOK"]
 
+
 @dataclass
 class session_analysis_result:
-	number_of_rising_sessions: int
-	number_of_falling_sessions: int
-	number_of_unchanged_sessions: int
+    number_of_rising_sessions: int
+    number_of_falling_sessions: int
+    number_of_unchanged_sessions: int
 
 @dataclass
 class statistical_analysis_result:
-	median: float
-	dominant: float
-	standard_deviation: float
-	coefficient_of_variation: float
+    median: float
+    dominant: float
+    standard_deviation: float
+    coefficient_of_variation: float
